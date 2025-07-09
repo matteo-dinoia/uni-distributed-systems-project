c7ac996ce9ec14ea6f90c01ee353d6a0178181d7
denniscattoni <dennis.cattoni@studenti.unitn.it>
2025-07-09 16:39:48 +0200

    added new classes for the new architecture

---
+++ src/main/java/base_state/BaseState.java
@@ -0,0 +1,17 @@
+package base_state;
+
+public class BaseState {
+
+    // Attributes
+
+    // Constructor
+    BaseState() {
+
+    }
+
+    //Methods
+    public void handleRecover() {
+
+    }
+
+}
---
+++ src/main/java/base_state/Crashed.java
@@ -0,0 +1,4 @@
+package base_state;
+
+public class Crashed {
+}
